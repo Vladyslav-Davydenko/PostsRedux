f998f19702fb60a51fd4f59e3450d1996d3e7dfa
Vladyslav <5vladik068@gmail.com>
2023-12-14 11:36:01 +0200

    Added required mutations to post

--- posts-rtk-query/src/helpers/posts/PostsSlice.ts
+++ posts-rtk-query/src/helpers/posts/PostsSlice.ts
@@ -47,10 +47,80 @@ export const extendedApiSlice = apiSlice.injectEndpoints({
         ];
       },
     }),
+    getPostByUserId: builder.query<EntityState<PostType, number>, void>({
+      query: (id) => `/posts/?userId=${id}`,
+      transformResponse: (responce: PostType[]) => {
+        let min = 1;
+        const loadedPosts = responce.map((post) => {
+          if (!post.date)
+            post.date = sub(new Date(), { minutes: min++ }).toISOString();
+          if (!post.reactions)
+            post.reactions = {
+              thumbsUp: 0,
+              wow: 0,
+              heart: 0,
+              rocket: 0,
+              coffee: 0,
+            };
+          return post;
+        });
+        return postsAdapter.setAll(initialState, loadedPosts);
+      },
+      providesTags: (result) => {
+        if (!result?.ids) return [{ type: "Post", id: "LIST" }];
+        return [...result.ids.map((id) => ({ type: "Post" as const, id }))];
+      },
+    }),
+    addNewPost: builder.mutation<Partial<PostType>, PostType>({
+      query: (initialPost) => ({
+        url: "/posts",
+        method: "POST",
+        body: {
+          ...initialPost,
+          userId: Number(initialPost.userId),
+          date: new Date().toISOString(),
+          reactions: {
+            thumbsUp: 0,
+            wow: 0,
+            heart: 0,
+            rocket: 0,
+            coffee: 0,
+          },
+        },
+      }),
+      invalidatesTags: [{ type: "Post", id: "LIST" }],
+    }),
+    updatePost: builder.mutation<Partial<PostType>, PostType>({
+      query: (updatedPost) => ({
+        url: "/posts",
+        method: "POST",
+        body: {
+          ...updatedPost,
+          date: new Date().toISOString(),
+        },
+      }),
+      invalidatesTags: (result, error, arg) => [{ type: "Post", id: arg.id }],
+    }),
+    deletePost: builder.mutation<Partial<PostType>, PostType>({
+      query: (id) => ({
+        url: `/posts/${id}`,
+        method: "DELETE",
+        body: {
+          id,
+        },
+      }),
+      invalidatesTags: (result, error, arg) => [{ type: "Post", id: arg.id }],
+    }),
   }),
 });
 
-export const { useGetPostsQuery } = extendedApiSlice;
+export const {
+  useGetPostsQuery,
+  useGetPostByUserIdQuery,
+  useAddNewPostMutation,
+  useDeletePostMutation,
+  useUpdatePostMutation,
+} = extendedApiSlice;
 
 export const selectPostResult = extendedApiSlice.endpoints.getPosts.select();
 
